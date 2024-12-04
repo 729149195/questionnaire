773792e6b425da3f18dce415bd25915e1376fe9c
729149195 <729149195@qq.com>
2024-12-04 22:03:59 +0800

    amazon

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,31 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  base: '/questionnaire/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/global.scss";`
+      }
+    }
   }
 })
